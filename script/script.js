73fa84d09901cc4b65e0d4853ab30ba4cb3e1071
Lidia <lida.nazarova94@gmail.com>
2022-01-25 23:07:56 +0300

    fixed project 1st review++

--- script/script.js
+++ script/script.js
@@ -1,12 +1,30 @@
-//  открыть редактирование профиля кликом по иконке ручка______________________
+//  открыть редактирование профиля кликом по иконке ручка с замесением контента в поле ввода___________
 let edit_button = document.querySelector('.profile__edit-button');
 let popup_window = document.querySelector('.popup');
 let popup_opened = document.querySelector('.popup_opened');
 
+let formElement = document.querySelector('.popup__field');
+let nameInput = document.querySelector('.popup__type_fullname');
+let jobInput = document.querySelector('.popup__type_profession');
+
+let profile = document.querySelector('.profile__text');
+let fullname = document.querySelector('.profile__title');
+let profession = document.querySelector('.profile__subtitle');
+
 function open_popup() {  
     popup_window.classList.add('popup_opened');
+    nameInput.value = fullname.textContent;
+    jobInput.value = profession.textContent;
 }
 edit_button.addEventListener('click', open_popup);
+// Занесение текста в форму и закрытие попапа с изменением контента__________
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    fullname.textContent = nameInput.value;
+    profession.textContent = jobInput.value;
+    close_popup();
+}
+formElement.addEventListener('submit', formSubmitHandler);
 //  закрыть редактирование профиля кликом по иконке крестик________________
 let popup_close = document.querySelector('.popup__close-icon');
 
@@ -14,21 +32,3 @@ function close_popup() {
     popup_window.classList.remove('popup_opened');
 }
 popup_close.addEventListener('click', close_popup);
-// Занесение текста в форму
-let formElement = document.querySelector('.popup__field');
-let fullname = document.querySelector('.profile__title');
-let profession = document.querySelector('.profile__subtitle');
-let nameInput = document.querySelector('.popup__type_fullname');
-let jobInput = document.querySelector('.popup__type_profession');
-
-nameInput.value = fullname.textContent;
-jobInput.value = profession.textContent;
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    fullname.value = nameInput.textContent;
-    profession.value = jobInput.textContent;
-}
-formElement.addEventListener('submit', formSubmitHandler);
-//  закрыть редактирование профиля кликом по кнопке сохранить_____________
-let popup_save = document.querySelector('.popup__save-button');
